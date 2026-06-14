ca69492b92529337e486990db11eda02d52967b0
tuzc0 <claudiojtz1402@gmail.com>
2026-06-14 02:15:21 -0600

    Resolve sonar issues in match gameplay refactor

--- Hangman.Business/Helpers/RandomCategorySelector.cs
+++ Hangman.Business/Helpers/RandomCategorySelector.cs
@@ -1,17 +1,35 @@
 ﻿using System;
+using System.Security.Cryptography;
 
 namespace Hangman.Business.Helpers
 {
     internal static class RandomCategorySelector
     {
-        private static readonly object RandomSyncRoot = new object();
-        private static readonly Random RandomGenerator = new Random();
-
         public static int GetRandomIndex(int maxExclusive)
         {
-            lock (RandomSyncRoot)
+            if (maxExclusive <= 0)
             {
-                return RandomGenerator.Next(maxExclusive);
+                throw new ArgumentOutOfRangeException(
+                    nameof(maxExclusive),
+                    "The maximum exclusive value must be greater than zero.");
+            }
+
+            using (RandomNumberGenerator randomNumberGenerator =
+                RandomNumberGenerator.Create())
+            {
+                byte[] randomBytes = new byte[4];
+
+                int maxValue = int.MaxValue - (int.MaxValue % maxExclusive);
+                int randomValue;
+
+                do
+                {
+                    randomNumberGenerator.GetBytes(randomBytes);
+                    randomValue = BitConverter.ToInt32(randomBytes, 0) & int.MaxValue;
+                }
+                while (randomValue >= maxValue);
+
+                return randomValue % maxExclusive;
             }
         }
     }
